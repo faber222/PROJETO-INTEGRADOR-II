0abe19e9c3ed957cd904291ec1dcdc738dd9440e
Faber Bernardo <74377748+faber222@users.noreply.github.com>
2023-10-27 14:30:09 +0000

    Adicionado função wireless

--- PlatformIO/Projects/EspSensors/src/main.cpp
+++ PlatformIO/Projects/EspSensors/src/main.cpp
@@ -18,8 +18,8 @@ ADS1115 ADS(0x48);
 float Temperature;
 float Humidity;
 
-const char* ssid;
-const char* password;
+const char* ssid = "espAcesso";
+const char* password = "12345678";
 
 WiFiServer server(80);
 
@@ -28,7 +28,7 @@ void setup() {
   delay(100);
   WiFi.begin(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED){
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
@@ -40,35 +40,42 @@ void setup() {
 
   pinMode(DHTPin, INPUT);
   // pinMode();
-  ADS.begin();
+  // ADS.begin();
   dht.begin();
 
   server.begin();
 }
 
 void loop() {
-  Temperature = dht.readTemperature(); // Obtém os valores da temperatura
-  Humidity = dht.readHumidity(); // Obtém os valores da umidade
+  if (WiFi.status() != WL_CONNECTED) {
+    WiFi.reconnect();
+    Serial.println("");
+    Serial.println("Wifi conectado!");
+    Serial.print("Endereço de ip: ");
+    Serial.println(WiFi.localIP());
+  } else {
+    Temperature = dht.readTemperature(); // Obtém os valores da temperatura
+    Humidity = dht.readHumidity(); // Obtém os valores da umidade
 
-  // ADS.setGain(0);
-  // int16_t val_0 = ADS.readADC(0);
-  // int16_t val_1 = ADS.readADC(1);
-  // int16_t val_2 = ADS.readADC(2);
-  // int16_t val_3 = ADS.readADC(3);
+    // ADS.setGain(0);
+    // int16_t val_0 = ADS.readADC(0);
+    // int16_t val_1 = ADS.readADC(1);
+    // int16_t val_2 = ADS.readADC(2);
+    // int16_t val_3 = ADS.readADC(3);
 
-  // float f = ADS.toVoltage(1);
-  
+    // float f = ADS.toVoltage(1);
 
+    Serial.print("Temperatura: ");
+    Serial.print(Temperature);
+    Serial.print("ºC");
+    Serial.println("");
 
-  Serial.print("Temperatura: ");
-  Serial.print(Temperature);
-  Serial.print("ºC");
-  Serial.println("");
+    Serial.print("Umidade: ");
+    Serial.print(Humidity);
+    Serial.print("g/m³");
+    Serial.println("");
+    delay(1000);
+  }
 
-  Serial.print("Umidade: ");
-  Serial.print(Humidity);
-  Serial.print("g/m³");
-  Serial.println("");
-  delay(1000);
 }
 
